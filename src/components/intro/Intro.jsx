32a81666f54ec11e1f82804173ffc4c86aadeeef
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-03-30 22:41:59 -0500

    Portfolio list and mostly complete intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,18 +1,33 @@
 import "./intro.scss"
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Educator", "Pit Master", "Musician"]
+    })
+  },[])
+
+
   return (
     <div className="intro" id="intro">
       <div className="left">
           <div className="imgContainer">
-              <img src="assets/profilepic.png" alt=""/>
+              <img src="assets/profilepictwo.png" alt=""/>
           </div>
       </div>
       <div className="right">
           <div className="wrapper">
             <h2> Hello! I'm</h2>
             <h1>Jose "Joey" Gonzales</h1>
-            <h3>Front-End Developer<span></span></h3>
+            <h3>Front-End Developer <span ref={textRef}></span></h3>
           </div>
           <a href="#portfolio">
               <img src="assets/downarrow.png" alt=""/>
