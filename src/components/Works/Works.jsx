9c2471f6c6704745ec1c8e778cd3e7ccb9b47d57
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-03-31 21:43:04 -0500

    updates on works page

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -3,7 +3,25 @@ import "./works.scss"
 export default function Works() {
   return (
     <div className="works" id="works">
-
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/workpic.png" alt=""/>
+                </div>
+                <h2>Title</h2>
+                <p>Insert Description here.</p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right">
+                <img src="" alt=""/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
